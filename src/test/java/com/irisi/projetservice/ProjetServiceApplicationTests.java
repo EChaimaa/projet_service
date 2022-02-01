31fab127fc87bad4a6006339b144f4ad00e1043f
EChaimaa <ezaghabchaimaa@gmail.com>
2022-02-02 00:23:19 +0100

    yml files for deplyment set

--- src/test/java/com/irisi/projetservice/ProjetServiceApplicationTests.java
+++ src/test/java/com/irisi/projetservice/ProjetServiceApplicationTests.java
@@ -3,10 +3,10 @@ package com.irisi.projetservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ProjetServiceApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
